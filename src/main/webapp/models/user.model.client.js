9e3161ed72845201339d5a9929466c14c8b85a89
Dhaval Dedhia <dhavl32@gmail.com>
2020-01-28 02:11:14 -0500

    Added user model

---
+++ src/main/webapp/models/user.model.client.js
@@ -0,0 +1,63 @@
+function User(_id, username, password, firstName, lastName, role) {
+    this.username = username;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.role = role;
+    this._id = _id;
+
+    function setUsername(username) {
+        this.username = username;
+    }
+
+    this.getUserName = function () {
+        return this.username;
+    }
+
+    function setPassword(password) {
+        this.password = password;
+    }
+
+    this.getPassword = function () {
+        return this.password;
+    }
+
+    function setFirstName(firstName) {
+        this.firstName = firstName;
+    }
+
+    this.getFirstName = function () {
+        return this.firstName;
+    }
+
+    function setLastName(lastName) {
+        this.lastName = lastName;
+    }
+
+    this.getLastName = function () {
+        return this.lastName;
+    }
+
+    function setRole(role) {
+        this.role = role;
+    }
+
+    this.getRole = function () {
+        return this.role;
+    }
+
+    this.getUserId = function () {
+        return this._id;
+    }
+
+    this.getUser = function () {
+        return {
+            username: this.username,
+            password: this.password,
+            firstName: this.firstName,
+            lastName: this.lastName,
+            role: this.role
+        }
+    }
+
+}
